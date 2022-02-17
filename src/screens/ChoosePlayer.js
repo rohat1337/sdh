348b2bbb8fcafc078f5dd646c09c58d16ba9fdc5
Rohat Bedikanli <rohatretki@icloud.com>
2022-02-17 23:45:05 +0100

    Flyttade omkring lite så nya filter alternativen får plats.

--- src/screens/ChoosePlayer.js
+++ src/screens/ChoosePlayer.js
@@ -49,7 +49,8 @@ function ChoosePlayer(props) {
             var position = Object.values(data["Position"])
             var age = Object.values(data["Age"])
             var minutes = Object.values(data["Minutes played"])
-            var list = zip(players, teams, position, age, minutes)
+            var height_cm = Object.values(data["Height"])
+            var list = zip(players, teams, position, age, minutes, height_cm)
             // For every player, create object
             // This is only needed because of format issues from flask (no object propety names to access)
             for (var player of list) {
@@ -84,52 +85,57 @@ function ChoosePlayer(props) {
             <ImageBackground style={styles.root} source={require('../imgs/iks.png')} resizeMode="cover">
 
                 <View style={styles.root_left}>
-                    <FlatList
-                    // Filter players by Name, Team, Age, Position and Minutes played
-                    data={players.filter((player) => (fix(player["Player"].toLowerCase()).includes(searchPlayer.toLowerCase()) && 
-                                                    player["Team within selected timeframe"].toLowerCase().includes(searchTeam.toLowerCase()) &&
-                                                    (player["Age"] >= minAge && player["Age"] <= maxAge) &&
-                                                    player["Position"].toLowerCase().includes(searchPosition.toLowerCase()) &&
-                                                    player["Minutes played"] >= minutesPlayed) &&
-                                                    (field.some(ele => player["Position"].toLowerCase().includes(ele)) || field.length === 0))}
-                    renderItem={({ item }) => {
-                        const textColor = selectedPlayers.includes(item.Player) ? "#ffe00f" : "white";
-                        return (   
-                            <View style={styles.players_TO}>                     
-                                <TouchableOpacity
-                                onPress={() => setPlayer(item.Player)}>
-                                    <View style={styles.players_V}>
-                                        <View style={styles.players_V_L}>
-                                            <Text style={[styles.text_L, {color: textColor}]}>{item["Player"]}</Text>
-                                        </View>
-                                        <View style={styles.players_V_R}>
-                                            <Text style={[styles.text_R, {color: textColor}]}>{item["Team within selected timeframe"]}</Text>
-                                            <Text style={[styles.text_R, {color: textColor}]}>, </Text>
-                                            <Text style={[styles.text_R, {color: textColor}]}>{item["Age"]}</Text>
-                                            <Text style={[styles.text_R, {color: textColor}]}>, </Text>
-                                            <Text style={[styles.text_R, {color: textColor}]}>{item["Position"]}</Text>
-                                        </View>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        )
-                    }}/>
-                </View>
-                <View style={styles.root_right}>
-                    <View style={styles.filters_U}>
-                        <TextInput 
+                    <TextInput 
                         placeholder="Sök spelare..."
                         placeholderTextColor="white"
                         style={styles.search}
                         onChangeText={setSearchPlayer}
                         value={searchPlayer}/>
+                    <View style={{height: "85%"}}>
+                        <FlatList
+                        // Filter players by Name, Team, Age, Position and Minutes played
+                        data={players.filter((player) => (fix(player["Player"].toLowerCase()).includes(searchPlayer.toLowerCase()) && 
+                                                        player["Team within selected timeframe"].toLowerCase().includes(searchTeam.toLowerCase()) &&
+                                                        (player["Age"] >= minAge && player["Age"] <= maxAge) &&
+                                                        player["Position"].toLowerCase().includes(searchPosition.toLowerCase()) &&
+                                                        player["Minutes played"] >= minutesPlayed) &&
+                                                        (field.some(ele => player["Position"].toLowerCase().includes(ele)) || field.length === 0))}
+                        renderItem={({ item }) => {
+                            const textColor = selectedPlayers.includes(item.Player) ? "#ffe00f" : "white";
+                            return (   
+                                <View style={styles.players_TO}>                     
+                                    <TouchableOpacity
+                                    onPress={() => setPlayer(item.Player)}
+                                    style={{ justifyContent: "center"}}>
+                                        
+                                        <View style={styles.players_V}>
+                                            <View style={styles.players_V_L}>
+                                                <Text style={[styles.text_L, {color: textColor}]}>{item["Player"]}</Text>
+                                            </View>
+                                            <View style={styles.players_V_R}>
+                                                <Text style={[styles.text_R, {color: textColor}]}>{item["Team within selected timeframe"]}</Text>
+                                                <Text style={[styles.text_R, {color: textColor}]}>, </Text>
+                                                <Text style={[styles.text_R, {color: textColor}]}>{item["Age"]}</Text>
+                                                <Text style={[styles.text_R, {color: textColor}]}>, </Text>
+                                                <Text style={[styles.text_R, {color: textColor}]}>{item["Position"]}</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            )
+                        }}/>
+                    </View>
+                </View>
+                <View style={styles.root_right}>
+                    <View style={styles.filters_U}>
+                        
                         <View style={styles.filters_UL}>
                             <TextInput 
                             placeholder="Sök lag..."
                             placeholderTextColor="white"
                             style={styles.search_small}
                             onChangeText={setTeam}/>
-                            <View style={{flex: 0.5, flexDirection: "column", height: "100%", alignItems: "center", marginLeft:"1%", marginTop: "2%"}}>
+                            <View style={{flex: 0.5, flexDirection: "column", alignItems: "center", marginLeft:"1%", marginBottom: "3%"}}>
                                 <View style={{flexDirection:"row"}}>
                                     <View>
                                         <View style={{flexDirection: "row",  width: windowWidth/10}}>
@@ -150,7 +156,7 @@ function ChoosePlayer(props) {
                                             onValueChange={value => setMinAge(parseInt(value))}>
                                         </Slider>
                                     </View>
-                                    <View style={{marginLeft: "10%"}}>
+                                    <View style={{marginLeft: "3%"}}>
                                         <View style={{flexDirection: "row", width: windowWidth/10}}>
                                             <Text style={styles.slider_text}>Ålder (max)</Text>
                                             <TextInput style={[styles.slider_text, {width: windowWidth/30}]}
@@ -178,14 +184,14 @@ function ChoosePlayer(props) {
                                 style={styles.search_small}
                                 onChangeText={setPosition}/>
                             <View style={{flex: 0.5, alignItems:"center"}}>
-                                <View style={{flexDirection:"row", paddingTop: "5%"}}>
+                                <View style={{flexDirection:"row"}}>
                                     <Text style={[styles.slider_text, {marginLeft: "15%"}]}>Spelade minuter</Text>
                                     <TextInput placeholder={0}
                                             value={minutesPlayed}
                                             style={styles.slider_text}
                                             onChangeText={value => setMinutesPlayed(value)}/>
                                 </View>
-                                <Slider style={{ width: windowWidth/4.5, height: windowHeight/20, marginLeft: "5%"}} 
+                                <Slider style={{ width: windowWidth/4.5, height: windowHeight/20, marginLeft: "5%", marginBottom: "6%"}} 
                                     minimumValue={0}
                                     maximumValue={1}
                                     minimumTrackTintColor="#078efb"
@@ -215,32 +221,31 @@ const styles = StyleSheet.create({
     root_left: {
         flex: 0.45,
         alignItems:"center",
-        marginBottom: "5%",
-        marginTop: "2%",
+        justifyContent: "space-between",
+        
     },
     root_right: {
         flex:0.55,
+        height: windowHeight - windowHeight/10,
     },
     players_TO: {
         width: windowWidth/3,
         marginLeft: windowWidth/20,
-        alignItems:"center",
-        height: "75%",
+        height: windowHeight/20,
         borderRadius: 100,
         backgroundColor: "#0059a1",
-
+        marginVertical: windowWidth/80
     },
     players_V: {
         flexDirection: "row",
         justifyContent: "space-between",
-        marginBottom: windowHeight/50
     },
     players_V_L: {
         flexDirection: "row",
         width: windowWidth/12,
         alignItems: "center",
         justifyContent: "center",
-        flex: 0.45
+        flex: 0.45,
     },
     players_V_R: {
         width: windowWidth/4,
@@ -255,25 +260,27 @@ const styles = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         fontSize: windowWidth/80,
-        fontFamily: "VitesseSans-Book"
+        fontFamily: "VitesseSans-Book",
+        marginBottom: "7%"
     },
     text_R: {
         color: "white",
         fontWeight: "bold",
         fontSize: windowWidth/100,
         fontFamily: "VitesseSans-Book",
+        marginBottom: "3.5%"
     },
     filters_U: {
-        flex: 0.7,
-        alignItems: "center"
+        height: "40%",
+        alignItems: "center",
     },
     filters_L: {
-        flex:0.3,
+        height: "60%",
         alignItems: "center",
         marginBottom: "30%",
-
     },
     search: {
+        marginLeft: "11%",
         paddingLeft: "2%",
         borderWidth: 1,
         borderColor: "black",
@@ -295,6 +302,7 @@ const styles = StyleSheet.create({
         width: "80%",
         height: "20%",
         marginVertical: "2%",
+        marginTop: "3.5%"
     },
     filters_TO: {
         backgroundColor: "#0059a1",
@@ -316,12 +324,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "black",
         borderRadius: 50,
-        width: "100%",
-        height: "50%",
+        height: "55%",
         fontSize: 17,
         fontWeight: "bold",
         backgroundColor: "gray",
-        marginTop: "5%",
         color: "white",
         fontFamily: "VitesseSans-Book",
         opacity: .9

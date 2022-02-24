052f6f43ecae38a287761aa622de5eab93421bb3
Tim Solig <tim.solig@gmail.com>
2022-02-24 08:45:20 +0100

    Add function headers

---
+++ src/server/data_PreProcessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+def openExcelFile():
+    df = pd.read_excel('playersAllsvenskan.xlsx')
+    return df
+
+
+
+def removeZeroEntries(df):
+    pass
+
+def contractEntriesToYears(df):
+    pass

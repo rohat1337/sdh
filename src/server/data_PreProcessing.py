bda48580cb65f4bbf87cb9c36b991d9977c36fd0
Tim Solig <tim.solig@gmail.com>
2022-03-02 20:23:32 +0100

    Create changePosName function, not implemented

--- src/server/data_PreProcessing.py
+++ src/server/data_PreProcessing.py
@@ -3,8 +3,66 @@ import numpy as np
 
 
 def openExcelFile():
+    
     df = pd.read_excel('playersAllsvenskan.xlsx')
+
+    # df["Positions"] = df["Position"].map(changePosName)
+    
     return df
+
+
+
+def changePosName(pos_string):
+    
+    result = []
+    
+    pos_array = pos_string.lower().replace(" ","").split(",")
+    
+    for pos in pos_array: 
+        print(pos)
+
+        if (pos == "gk"):
+            result.append("MV")
+        
+
+        elif (pos == "cf"):
+            result.append("9")
+        
+
+        elif (pos == "lw" or pos == "lmf" or pos == "lamf" or pos == "lwf"):
+            result.append("7(v)")
+        
+
+        elif (pos == "rw" or pos == "rmf" or pos == "ramf" or pos == "rwf"):
+            result.append("7(h)")
+
+
+        elif (pos == "amf"):
+            result.append("10")
+        
+
+
+        elif (pos == ("lcmf") or pos == ("rcmf")):
+            result.append("8")
+        
+
+        elif (pos == ("dmf") or pos == ("ldmf") or pos == ("rdmf")):
+            result.append("6")
+        
+
+        elif (pos == "lb" or pos == ("lwb")):
+            result.append("WB (v)")
+        
+
+        elif (pos == "rb" or pos == ("rwb")):
+            result.append("WB (h)")
+        
+
+
+        elif (pos == "lcb" or pos == "rcb" or pos == "cb"): 
+            result.append("MB")
+        
+    return ",".join(result) 
 
 
 

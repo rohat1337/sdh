ab231e3cbe487faf691a1048706866820add989f
Rohat Bedikanli <rohatretki@icloud.com>
2022-06-25 18:23:57 +0200

    Add footer

---
+++ src/components/MyText.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function MyText(props) {
+    return (
+        <Text style={[props.style, { fontFamily: 'VitesseSans-Book' }]}>{props.text}</Text>
+    )
+}
